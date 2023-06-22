2dccaf770ef67c1cc1c8144186a615cd2062c90f
joaovitorlemos <joaovitorlemos2015@gmail.com>
2023-06-21 22:01:10 -0300

    adding knex file

--- knexfile.js
+++ knexfile.js
@@ -6,6 +6,13 @@ module.exports = {
     connection: {
       filename: path.resolve(__dirname, "src", "database", "database.db")
     },
+    pool: {
+      // Adicionando essa funcionalidade para conseguir deletar em "CASCADE" no SQLite. Porque por padrão no SQLite essa função está desabilitada.
+      afterCreate: (connection, callback) => connection.run("PRAGMA foreign_keys = ON", callback)
+    },
+    migrations: {
+      directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
+    },
     useNullAsDefault: true
   }
 };
